97001388bfcaf7cf950fae3c47881fc5898cf0ff
WXenon <ng_weixiong@yahoo.com>
2020-02-13 17:12:21 +0800

    update add msal

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'userLandingPage.dart';
 import 'adminLandingPage.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
-//import 'package:msal_mobile/msal_mobile.dart';
+import 'package:msal_mobile/msal_mobile.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'msal_p'
 
 void main() => runApp(UASReg());
 
@@ -40,17 +42,17 @@ class _LoginPage extends State<LoginPage> {
 
   TextEditingController usernameController = new TextEditingController(), pwdController = new TextEditingController();
 
-//  MsalMobile msal;
-//
-//  @override
-//  void initState() {
-//    super.initState();
-//    MsalMobile.create('assets/auth_config.json', "https://login.microsoftonline.com/Organizations").then((client) {
-//      setState(() {
-//        msal = client;
-//      });
-//    });
-//  }
+  MsalMobile msal;
+
+  @override
+  void initState() {
+    super.initState();
+    MsalMobile.create('assets/auth_config.json', "https://login.microsoftonline.com/Organizations").then((client) {
+      setState(() {
+        msal = client;
+      });
+    });
+  }
 
   _launchURL() async {
     final url = 'https://uasrdblogin.azurewebsites.net';
@@ -92,77 +94,78 @@ class _LoginPage extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Align(
-              child: new Container(
-                margin: EdgeInsets.all(32.0),
-                  child: new TextField(
-                      controller: usernameController,
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18.0,
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                      cursorColor: Colors.blue,
-                      decoration: new InputDecoration(
-                        labelText: 'Email',
-                        icon: new Icon(Icons.email),
-                        hintText: 'Email',
-                      )
-                  )
-              )
-            ),
-            Align(
-                child: new Container(
-                    margin: EdgeInsets.fromLTRB(32, 0, 32, 32),
-                    child: new TextField(
-                        controller: pwdController,
-                        textAlign: TextAlign.start,
-                        obscureText: true,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18.0,
-                        ),
-                        keyboardType: TextInputType.visiblePassword,
-                        cursorColor: Colors.blue,
-                        decoration: new InputDecoration(
-                          labelText: 'Password',
-                          icon: new Icon(Icons.vpn_key),
-                          hintText: 'Password',
-                        )
-                    )
-                )
-            ),
-            Align(
-                child: new Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Align(
-                        child: new Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 70, 0),
-                          child: new RaisedButton(onPressed: (){
-                            login(usernameController.text, pwdController.text);
-                          }, child: new Text('Login')),
-                        )
-                    ),
-                    Align(
-                        child: new Container(
-                          child: new RaisedButton(onPressed: null, child: new Text('Sign Up')),
-                        )
-                    ),
-                  ],
-                )
-            )
-          ],
-        ),
-      ),
+      body: ,
+//      appBar: AppBar(
+//        title: Text(widget.title),
+//      ),
+//      body: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.start,
+//          children: <Widget>[
+//            Align(
+//              child: new Container(
+//                margin: EdgeInsets.all(32.0),
+//                  child: new TextField(
+//                      controller: usernameController,
+//                      textAlign: TextAlign.start,
+//                      style: TextStyle(
+//                        color: Colors.black,
+//                        fontSize: 18.0,
+//                      ),
+//                      keyboardType: TextInputType.emailAddress,
+//                      cursorColor: Colors.blue,
+//                      decoration: new InputDecoration(
+//                        labelText: 'Email',
+//                        icon: new Icon(Icons.email),
+//                        hintText: 'Email',
+//                      )
+//                  )
+//              )
+//            ),
+//            Align(
+//                child: new Container(
+//                    margin: EdgeInsets.fromLTRB(32, 0, 32, 32),
+//                    child: new TextField(
+//                        controller: pwdController,
+//                        textAlign: TextAlign.start,
+//                        obscureText: true,
+//                        style: TextStyle(
+//                          color: Colors.black,
+//                          fontSize: 18.0,
+//                        ),
+//                        keyboardType: TextInputType.visiblePassword,
+//                        cursorColor: Colors.blue,
+//                        decoration: new InputDecoration(
+//                          labelText: 'Password',
+//                          icon: new Icon(Icons.vpn_key),
+//                          hintText: 'Password',
+//                        )
+//                    )
+//                )
+//            ),
+//            Align(
+//                child: new Row(
+//                  mainAxisAlignment: MainAxisAlignment.center,
+//                  children: <Widget>[
+//                    Align(
+//                        child: new Container(
+//                          margin: EdgeInsets.fromLTRB(0, 0, 70, 0),
+//                          child: new RaisedButton(onPressed: (){
+//                            login(usernameController.text, pwdController.text);
+//                          }, child: new Text('Login')),
+//                        )
+//                    ),
+//                    Align(
+//                        child: new Container(
+//                          child: new RaisedButton(onPressed: null, child: new Text('Sign Up')),
+//                        )
+//                    ),
+//                  ],
+//                )
+//            )
+//          ],
+//        ),
+//      ),
     );
   }
 }//login page logic
